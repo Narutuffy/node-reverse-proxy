4537541e3faf5732f50624c6fe8af493ce480c63
Vamsi Settypalli <s.vamsirao@gmail.com>
2020-01-06 17:52:02 +0530

    WIP: proxying requests

--- app.js
+++ app.js
@@ -1,11 +1,11 @@
 var http = require('http');
-var port = 5000;
+var port = 8888;
 
 http.createServer(function (clientReq, clientRes) {
   console.log('serve: ' + clientReq.url);
 
   var options = {
-    hostname: 'planokay.com',
+    hostname: '104.22.9.217',
     port: 80,
     path: clientReq.url,
     method: clientReq.method,
@@ -13,7 +13,8 @@ http.createServer(function (clientReq, clientRes) {
   };
 
   var proxy = http.request(options, function (res) {
-    clientRes.writeHead(res.statusCode, res.headers)
+    clientRes.writeHead(res.statusCode, res.headers);
+    console.log(clientRes);
     res.pipe(clientRes, {
       end: true
     });
@@ -23,5 +24,12 @@ http.createServer(function (clientReq, clientRes) {
     end: true
   });
 }).listen(port);
-// /////////////////////////////////
 
+// /////////////////////////////////
+// Proxy using request module
+// var request = require('request');
+// http.createServer(function(clientReq, clientRes) {
+//   // console.log("======>",clientReq);
+//   var url = `https://planokay.com`
+//   clientReq.pipe(request({ qs:clientReq.query, uri: url, json: true })).pipe(clientRes);
+// }).listen(port);
